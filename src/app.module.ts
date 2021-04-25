edcae838c98f7f20d25b0500038ea01cdeeb0830
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-04-25 23:34:08 +0200

    Database works

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { CollectionModule } from './collection/api/collection.module';
+import * as Joi from '@hapi/joi';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './collection/infrastructure/data-source/postgres/database.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CollectionModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
