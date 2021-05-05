20361154e8d47ae24d7234d1b4949882f5e54f03
scupak <anto1095@easv365.dk>
2021-05-05 09:37:26 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
-  
+
 
 }
 bootstrap();
