4b6e2098936ef9f0770b27799c63673dbe7d8311
Andreas <55873124+CloseToDie@users.noreply.github.com>
2021-05-18 14:25:37 +0200

    Should work on Heroku (Maybe?)

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const configService: ConfigService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get('PORT') || 8080);
 }
 bootstrap();
